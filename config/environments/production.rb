2866cf2f7137acda23c55da6212caa5e0163e485
e047258 <ruben.dominguez@bbva.com>
2017-03-16 18:44:05 +0100

    change aws s3 url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,11 +83,10 @@ Rails.application.configure do
   config.paperclip_defaults = {
   storage: :s3,
   s3_credentials: {
-    bucket: 'fitcontest-production',
-    access_key_id: 'AKIAJCHT57ZUSQKIKMMQ',
-    secret_access_key: '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',
-    s3_region: 'eu-west-2',
+    bucket: ENV.fetch('S3_BUCKET_NAME'),
+    access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+    secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+    s3_region: ENV.fetch('AWS_REGION'),
   }
 }
-
 end
---
+++ config/initializers/paperclip.rb
@@ -0,0 +1,2 @@
+# config/initializers/paperclip.rb
+Paperclip::Attachment.default_options[:url] = 'fitcontest-production.s3.amazonaws.com'
